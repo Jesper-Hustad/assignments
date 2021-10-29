33b940a7334654910550ff9c2cc5c1433a1ae7e2
Jesper Hustad <jesterbe3@gmail.com>
2021-10-29 13:19:40 +0200

    8

--- ml_2502/8/2.py
+++ ml_2502/8/2.py
@@ -1,130 +1,209 @@
-"""
-Inspired from Jose Maynez https://medium.com/@flomay/using-q-learning-to-solve-the-cartpole-balancing-problem-c0a7f47d3f9d
-"""
-
-import gym
-import math
 import numpy as np
-from sklearn.preprocessing import KBinsDiscretizer
+
+"""
+Inspired from Jeremy Zhang https://towardsdatascience.com/implement-grid-world-with-q-learning-51151747b455
+"""
+
+BOARD_ROWS = 3
+BOARD_COLS = 4
+WIN_STATE = (0, 3)
+LOSE_STATE = (1, 3)
+START = (2, 0)
+DETERMINISTIC = False
 
 
-class CartPoleQAgent():
-    def __init__(self, buckets=(6, 12),
-                 num_episodes=500, min_lr=0.1,
-                 min_epsilon=0.1, discount=1.0, decay=25):
-        self.num_episodes = num_episodes
-        self.min_lr = min_lr
-        self.min_epsilon = min_epsilon  # The percentage you want to explore
-        self.discount = discount
-        self.decay = decay
-        self.buckets = buckets
+class State:
+    def __init__(self, state=START):
+        self.board = np.zeros([BOARD_ROWS, BOARD_COLS])
+        self.board[1, 1] = -1
+        self.state = state
+        self.isEnd = False
+        self.determine = DETERMINISTIC
 
-        self.env = gym.make('CartPole-v1')
-        self.steps = np.zeros(self.num_episodes)
-
-        # This is the action-value function being initialized to 0's
-        self.Q_table = np.zeros(self.buckets + (self.env.action_space.n,))
-
-        print("aaa", self.buckets + (self.env.action_space.n,))
-        print(f"{self.Q_table = }")
-
-        # Relevant for discretizer, with the observation on the form [position, velocity, angle, angular velocity]
-        # self.upper_bounds = [self.env.observation_space.high[0], 0.5, self.env.observation_space.high[2], math.radians(50)]
-        # self.lower_bounds = [self.env.observation_space.low[0], -0.5, self.env.observation_space.low[2], -math.radians(50)]
-        self.upper_bounds = [self.env.observation_space.high[2], math.radians(50)]
-        self.lower_bounds = [self.env.observation_space.low[2], -math.radians(50)]
-
-    def get_epsilon(self, t):
-        # Ensures that there's almost at least a min_epsilon chance of randomly exploring, declines as we advance in episodes
-        return max(self.min_epsilon, min(1., 1. - math.log10((t + 1) / self.decay)))
-
-    def get_learning_rate(self, t):
-        # Learning rate declines as we add more episodes
-        return max(self.min_lr, min(1., 1. - math.log10((t + 1) / self.decay)))
-
-    def discretize_state(self, observation):
-        """Convert continous state intro a discrete state"""
-        position, velocity, angle, angle_velocity = observation
-        est = KBinsDiscretizer(n_bins=self.buckets,
-                               encode='ordinal', strategy='uniform')
-        est.fit([self.lower_bounds, self.upper_bounds])
-        # return tuple(map(int, est.transform([[position, velocity, angle, angle_velocity]])[0]))
-        return tuple(map(int, est.transform([[angle, angle_velocity]])[0]))
-
-    def choose_action(self, state):
-        # Chooses either a random action (exploration) or the action based on the best Q value from the Q-table (exploiting)
-        if (np.random.random() < self.epsilon):
-            return self.env.action_space.sample()
+    def giveReward(self):
+        if self.state == WIN_STATE:
+            return 1
+        elif self.state == LOSE_STATE:
+            return -1
         else:
-            return np.argmax(self.Q_table[state])
+            return 0
 
-    def update_q(self, state, action, reward, new_state):
-        # Updates Q-table using the Q-formula.
-        self.Q_table[state][action] += (self.learning_rate *
-                                        (reward
-                                         + self.discount *
-                                         np.max(self.Q_table[new_state])
-                                         - self.Q_table[state][action]))
+    def isEndFunc(self):
+        if (self.state == WIN_STATE) or (self.state == LOSE_STATE):
+            self.isEnd = True
 
-    def train(self, print_steps=False, render=False):
-        '''
-        Trains agent making it go through the environment and choose actions based on its Q-table
-        Builds the Q-table as it trains
-        '''
-        # Looping through each episode
-        for e in range(self.num_episodes):
-            # Initializes the state
-            current_state = self.discretize_state(self.env.reset())
-            self.learning_rate = self.get_learning_rate(e)
-            self.epsilon = self.get_epsilon(e)
-            done = False
+    def _chooseActionProb(self, action):
+        if action == "up":
+            return np.random.choice(["up", "left", "right"], p=[0.8, 0.1, 0.1])
+        if action == "down":
+            return np.random.choice(["down", "left", "right"], p=[0.8, 0.1, 0.1])
+        if action == "left":
+            return np.random.choice(["left", "up", "down"], p=[0.8, 0.1, 0.1])
+        if action == "right":
+            return np.random.choice(["right", "up", "down"], p=[0.8, 0.1, 0.1])
 
-            # Looping through each step
-            while not done:
-                if render:
-                    self.env.render()
-                self.steps[e] += 1
-                # Choose Action (A) from State (S)
-                action = self.choose_action(current_state)
-                # Run the action in the enviroment (gym)
-                observation, reward, done, _ = self.env.step(action)
-                # Create new S (angle, angle_velocity)
-                new_state = self.discretize_state(observation)
-                # Update Q(S,A)
-                self.update_q(current_state, action, reward, new_state)
-                current_state = new_state
-                # When done is true we break out and continue to the next episode
-            if print_steps:
-                print(f"Trainingsession {e+1}:", int(self.steps[e]), "steps")
-        print('Finished training!')
+    def nxtPosition(self, action):
+        """
+        action: up, down, left, right
+        -------------
+        0 | 1 | 2| 3|
+        1 |
+        2 |
+        return next position on board
+        """
+        if self.determine:
+            if action == "up":
+                nxtState = (self.state[0] - 1, self.state[1])
+            elif action == "down":
+                nxtState = (self.state[0] + 1, self.state[1])
+            elif action == "left":
+                nxtState = (self.state[0], self.state[1] - 1)
+            else:
+                nxtState = (self.state[0], self.state[1] + 1)
+            self.determine = False
+        else:
+            # non-deterministic
+            action = self._chooseActionProb(action)
+            self.determine = True
+            nxtState = self.nxtPosition(action)
 
-    def run(self):
-        # Runs an episode while displaying the cartpole enviroment
-        steps = 0
-        done = False
-        current_state = self.discretize_state(self.env.reset())
-        while not done:
-            self.env.render()
-            steps += 1
-            action = self.choose_action(current_state)
-            observation, reward, done, _ = self.env.step(action)
-            new_state = self.discretize_state(observation)
-            current_state = new_state
-        return steps
+        # if next state is legal
+        if (nxtState[0] >= 0) and (nxtState[0] <= 2):
+            if (nxtState[1] >= 0) and (nxtState[1] <= 3):
+                if nxtState != (1, 1):
+                    return nxtState
+        return self.state
+
+    def showBoard(self):
+        self.board[self.state] = 1
+        for i in range(0, BOARD_ROWS):
+            print('-----------------')
+            out = '| '
+            for j in range(0, BOARD_COLS):
+                if self.board[i, j] == 1:
+                    token = '*'
+                if self.board[i, j] == -1:
+                    token = 'z'
+                if self.board[i, j] == 0:
+                    token = '0'
+                out += token + ' | '
+            print(out)
+        print('-----------------')
 
 
-def main():
-    agent = CartPoleQAgent(num_episodes=300)
-    # Training
-    print(f"Trains for {agent.num_episodes} episodes")
-    agent.train(print_steps=True, render=False)
-    # Showcase
-    input("Press Enter to showcase one episode with the trained model")
-    print("Number of steps:", agent.run())
+class Agent:
 
-    # Cartpole v0: max_episode_steps=200
-    # Cartpole v1: max_episode_steps=500
+    def __init__(self):
+        self.states = []  # record position and action taken at the position
+        self.actions = ["up", "down", "left", "right"]
+        self.State = State()
+        self.isEnd = self.State.isEnd
+        self.lr = 0.2
+        self.exp_rate = 0.3
+        self.decay_gamma = 0.9
+
+        # initial Q values
+        self.Q_values = {}
+        for i in range(BOARD_ROWS):
+            for j in range(BOARD_COLS):
+                self.Q_values[(i, j)] = {}
+                for a in self.actions:
+                    self.Q_values[(i, j)][a] = 0  # Q value is a dict of dict
+
+    def chooseAction(self):
+        # choose action with most expected value
+        mx_nxt_reward = 0
+        action = ""
+
+        if np.random.uniform(0, 1) <= self.exp_rate:
+            action = np.random.choice(self.actions)
+        else:
+            # greedy action
+            for a in self.actions:
+                current_position = self.State.state
+                nxt_reward = self.Q_values[current_position][a]
+                if nxt_reward >= mx_nxt_reward:
+                    action = a
+                    mx_nxt_reward = nxt_reward
+            # print("current pos: {}, greedy aciton: {}".format(self.State.state, action))
+        return action
+
+    def takeAction(self, action):
+        position = self.State.nxtPosition(action)
+        # update State
+        return State(state=position)
+
+    def reset(self):
+        self.states = []
+        self.State = State()
+        self.isEnd = self.State.isEnd
+
+    def play(self, rounds=10):
+        i = 0
+        while i < rounds:
+            # to the end of game back propagate reward
+            if self.State.isEnd:
+                # back propagate
+                reward = self.State.giveReward()
+                for a in self.actions:
+                    self.Q_values[self.State.state][a] = reward
+                print("Game End Reward", reward)
+                for s in reversed(self.states):
+                    current_q_value = self.Q_values[s[0]][s[1]]
+                    reward = current_q_value + self.lr * (self.decay_gamma * reward - current_q_value)
+                    self.Q_values[s[0]][s[1]] = round(reward, 3)
+                self.reset()
+                i += 1
+            else:
+                action = self.chooseAction()
+                # append trace
+                self.states.append([(self.State.state), action])
+                print("current position {} action {}".format(self.State.state, action))
+                # by taking the action, it reaches the next state
+                self.State = self.takeAction(action)
+                # mark is end
+                self.State.isEndFunc()
+                print("nxt state", self.State.state)
+                print("---------------------")
+                self.isEnd = self.State.isEnd
+
+    def show(self):
+        self.State.showBoard()
+        state = self.State
+        for i in range(0, BOARD_ROWS):
+            print('-----------------')
+            out = '| '
+            for j in range(0, BOARD_COLS):
+                if state.board[i, j] == 1:
+                    token = 'S'
+                if state.board[i, j] == -1:
+                    token = 'L'
+                if (i, j) == WIN_STATE:
+                    token = 'W'
+                if state.board[i, j] == 0:
+                    mx_nxt_reward = 0
+                    action = ""
+
+                    for a in self.actions:
+                            current_position = (i, j)
+                            nxt_reward = self.Q_values[current_position][a]
+                            if nxt_reward >= mx_nxt_reward:
+                                action = a
+                                mx_nxt_reward = nxt_reward
+                    moves = ["↑","↓","←","→","0","6","7","8","9","10","11"]
+                    move_position = ["up", "down", "left", "right", ""].index(action)
+                    token = moves[move_position]
+                out += token + ' | '
+            print(out)
+        print('-----------------')
 
 
 if __name__ == "__main__":
-    main()
+    ag = Agent()
+    print("initial Q-values ... \n")
+    print(ag.Q_values)
+
+    ag.play(50)
+    # print("latest Q-values ... \n")
+    ag.show()
+    # print(ag.Q_values)
